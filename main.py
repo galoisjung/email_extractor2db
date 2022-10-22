aafbf3a13e4adfdba17e860c61e3b7772690629c
Tarbuck <galoisjung@gmail.com>
2022-10-22 13:48:26 +0900

    make_main_function

---
+++ main.py
@@ -0,0 +1,9 @@
+import mail_extraction
+import naver_extraction
+
+mail_extraction.ham_extraction()
+mail_extraction.spam_extraction()
+
+naver_extraction.ham_extraction()
+naver_extraction.spam_extraction()
+
